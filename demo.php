c4f618aa8f94d9278384a22a429c482d32c7021c
Md Ali Ahsan Rana <rana_cse_ruet@yahoo.com>
2014-12-07 21:42:48 -0500

    * fix name conflict

--- demo.php
+++ demo.php
@@ -56,7 +56,7 @@
 					<select id="language">
 						<option value="">Select language</option>
 					</select>
-					<span>Logged in As: <label id="userId"></label>&nbsp;&nbsp;&nbsp;<a href="">logout</a></span>
+					<span>Logged in As: <label id="username"></label>&nbsp;&nbsp;&nbsp;<a href="">logout</a></span>
 				</div>
 				<div id="groupBox" class="row top10">
 					
