cf68654aefc591b806bbcc57b3848dc8c8842b5e
Md Ali Ahsan Rana <rana_cse_ruet@yahoo.com>
2014-12-23 01:45:01 -0500

    * remove php dependency(header.php still to go).

--- header.php
+++ header.php
@@ -22,7 +22,7 @@ $base_url = "http://hackathon/";
 					<a href="<?php echo $base_url;?>#about">About</a>
 				</li>
 				<li>
-					<a href="<?php echo $base_url;?>demo.php">Try It!</a>
+					<a href="<?php echo $base_url;?>demo.html">Try It!</a>
 				</li>
 				<li>
 					<a href="<?php echo $base_url;?>#contact">Contact</a>
--- js/ultra.js
+++ js/ultra.js
@@ -1,298 +1,275 @@
-/**
- * Created by Rana on 14-12-06.
- */
 
-var rChat;
-var translator;
-var robotSpeaker;
+function LanguageController($scope, $rootScope, translator) {
+	$scope.languages = [{"key":null, "value":"Select language"}];
+	$scope.lang = null;
+	//TODO error: initial value isn't being selected
+	translator.getLanguageList(function(languages){
+		$scope.$apply(function () {
+			angular.forEach(languages, function(language, key) {
+				$scope.languages.push({"key":key, "value":language});
+			});
+		});
+	});
 
-$(document).ready(function(){
-    translator     = new YTranslator("trnsl.1.1.20141130T053443Z.abe0172019286ab4.38cf8c2055843d9fa61079da020e63286c7c5dcf");
-    rChat     = new ULTraChat(translator);
+	$scope.languageChanged = function(){
+		console.log("language just changed");
+		$rootScope.$broadcast("languageChanged");
+		//TODO haven't caught anywhere yet/validation
+	}
+}
 
-    $('.intro-modal').modal({ backdrop: 'static', keyboard: false });
-    $("#joinBtn").click(tryLogin);
-    //console.log("Browser:"+platform.name);
-    if(platform && platform.name.toString() != "Chrome") {
-        //console.log("here");
-        $(".compatibility-warning").show().removeClass("hidden");
-        return;
-    }
-    robotSpeaker = new RobotSpeaker();
-    $("#private-section").css('position', 'absolute').css('bottom','0px').css('width','100%').css('z-index','1000');
+function LoginBoxController($scope, $modal){
+
+	var modalInstance = $modal.open({
+		templateUrl: 'loginModal',
+		controller: 'LoginAttemptController',
+		size: 'md',
+		backdrop :'static'
+	});
+}
+
+function LoginAttemptController($scope, $modalInstance, uchat, $rootScope,getIdentity) {
+	$scope.tryLogin = function(){
+		if ((!$scope.identity) || ($scope.identity.length < 3)){
+			return alert("username must be at least 3 letter");
+		}
+		uchat.connect($scope.identity, function(status) {
+			if (!status) {
+				console.err("Couldn't connect");
+				return false;
+			}
+			else {
+				console.log("Connection successfull!");
+				$modalInstance.close();
+				getIdentity.dataIdentity = $scope.identity;
+				$rootScope.$broadcast("loginSuccess");
+			}
+		});
+	}
+}
+
+function GroupController($scope, $rootScope, uchat, getIdentity) {
+
+	$scope.$on("loginSuccess", function(){
+		$scope.identity = getIdentity.dataIdentity;
+		uchat.joinGroup("UpStageCoder",
+			function memberJoined(member) {
+				//TODO member joined
+				$rootScope.$broadcast("newMemberJoin", member);
+			}, function memberLeaved(member) {
+				//TODO member leaved
+				$rootScope.$broadcast("memberLeave", member);
+			}, function (status) {
+				if (status) {
+					console.log("Joined group successfully");
+					$rootScope.$broadcast("groupJoinSuccess");
+				}
+				else {
+					console.log("group join attempt failed");
+				}
+		});
+		
+	});
+
+}
+
+function UserListController($scope, $rootScope, uchat, getIdentity){
+	$scope.users = [];
+	
+	$scope.addUser = function(user){
+		$scope.$apply(function () {
+			$scope.users.push(user.id);
+		});
+	};
+	$scope.removeUser = function(user){
+		$scope.$apply(function () {
+			$rootScope.$broadcast("closePrivateChatBox",user.id);
+			$scope.users.splice($scope.users.indexOf(user.id), 1);
+		});
+	};
+	$scope.openPrivateChat = function(userId) {
+		console.log("Trying private chat: "+userId);
+		//TODO not yet being listened
+		$rootScope.$broadcast("privateChatAttempt",userId);
+	};
+	$scope.$on("groupJoinSuccess", function(){
+		console.log("loading user list");
+		$scope.identity = getIdentity.dataIdentity;
+		uchat.getGroupMembers(function(members){
+			angular.forEach(members, function(member, index) {
+				
+				if(member.getEndpoint().id!=$scope.identity)
+				$scope.addUser(member.getEndpoint());
+			});
+		});
+	});
+	$scope.$on("newMemberJoin", function(event, member){
+		//console.log("new member id: "+member.id);
+		$scope.addUser(member.getEndpoint());
+	});
+	$scope.$on("memberLeave", function(event, member){
+		//console.log("Leaving member id: "+member.getEndpoint()));
+		$scope.removeUser(member.getEndpoint());
+	});
+}
+
+function GroupMessageController($scope, $rootScope, uchat) {
+	$scope.messages = [];
+
+	$scope.textToSend = "";
+
+	//TODO catch language change event
+	$scope.lang = "en";
+	
+	$scope.sendGroupMessage = function() {
+		console.log("sending text: "+$scope.textToSend);
+		var messageObj = {};
+		messageObj["message"] = $scope.textToSend;
+		messageObj["lang"]    = $scope.lang;
+		messageObj["type"]    = 'text';
+		messageObj["genre"]   = 'group';
+		messageObj["timestamp"]   = Date.now();
+		//console.log(messageObj);
+		$scope.textToSend = "";
+		uchat.sendGroupMessage(messageObj, $scope.loadGroupMessageHistory);
+	};
+	
+	$scope.loadGroupMessageHistory = function(sender, msg){
+		console.log("loading to message history");
+		msg.sender = sender;
+		
+		$scope.$apply(function () {
+			var timestamp = new Date(msg.timestamp);
+			var newDate   = new Date();
+			newDate.setTime(timestamp);
+			var dateString = newDate.toLocaleTimeString();
+			msg.timestamp  = dateString;
+			$scope.messages.push(msg);
+			console.log(msg);
+		});
+	};
+	
+	$scope.loadPrivateMessageHistory = function(sender, receiver, msg){
+		$rootScope.$broadcast("onLoadPrivateMessageHistory",sender, receiver, msg);
+	};
+	uchat.onMessage($scope.loadGroupMessageHistory,$scope.loadPrivateMessageHistory);
+}
+
+
+function PrivateChatBoxesController($scope, $rootScope, uchat) {
+	$scope.boxes = [{class: "private-chat-rana"}];
+}
+
+function PrivateMessageController($scope, $rootScope, uchat) {
+	//$scope.prototype = new GroupMessageController($scope, $rootScope, uchat);
+	$scope.boxes = {}; 
+	$scope.textToSend ={};
+	
+	//TODO catch language change event
+	$scope.lang = "en";
+	$scope.joinPrivateChat = function(userId){
+		uchat.joinPrivateChat(userId, function(){
+			
+			if(!$scope.boxes[userId]){
+				
+				$scope.boxes[userId]= {"messages":[]};
+				$scope.textToSend[userId] = "";
+			}
+			
+			//initAudioListener(userId);
+		});
+	};
+	
+	$scope.$on("privateChatAttempt",function(event,userId){
+		$scope.joinPrivateChat(userId);
+	});
+	
+	$scope.sendPrivateMessage = function(userId) {
+		
+		var messageObj = {};
+		messageObj["message"] = $scope.textToSend[userId];
+		messageObj["lang"]    = $scope.lang;
+		messageObj["type"]    = 'text';
+		messageObj["genre"]   = 'private';
+		messageObj["timestamp"]   = Date.now();
+		$scope.textToSend[userId] = "";
+		uchat.sendPrivateMessage(messageObj, userId, $scope.loadPrivateMessageHistory);
+	};
+	
+	$scope.loadPrivateMessageHistory = function(sender, receiver, msg,listenMsg){
+		
+		/*
+		if(messageObj.type == "voice" && sender != "Me") {
+			robotSpeaker.speak(messageObj.lang, messageObj.message);
+		}*/
+		msg.sender = sender;
+		
+		if(listenMsg){
+			if(!$scope.boxes[receiver]){
+				$scope.joinPrivateChat(receiver);
+			}
+			msg.sender = receiver;
+		}
+		
+		$scope.$apply(function () {
+			
+			var timestamp = new Date(msg.timestamp);
+			var newDate   = new Date();
+			newDate.setTime(timestamp);
+			var dateString = newDate.toLocaleTimeString();
+			msg.timestamp  = dateString;
+			$scope.boxes[receiver].messages.push(msg);
+		});
+	};
+	
+	$scope.closeBox = function(chatIdentity){
+		if($scope.boxes[chatIdentity])
+			delete $scope.boxes[chatIdentity];
+	};
+	
+	$scope.$on("closePrivateChatBox", function(event, chatIdentity){
+		$scope.closeBox(chatIdentity);
+	});
+	
+	$scope.$on("onLoadPrivateMessageHistory", function(event, sender, receiver, msg){
+		$scope.loadPrivateMessageHistory(receiver, sender, msg, true);
+	});
+}
+
+
+var app = angular.module("chat",['ui.bootstrap'])
+.config(function($provide) {
+	var translator = new YTranslator("trnsl.1.1.20141130T053443Z.abe0172019286ab4.38cf8c2055843d9fa61079da020e63286c7c5dcf");
+	$provide.factory('translator', function(){
+		return translator;
+	});
+	$provide.factory('uchat', function(){
+		return new ULTraChat(translator);
+	});
 });
 
-function tryLogin()
-{
-    var identity = $("#identity").val();
-    if ((!identity) || (identity.length < 3)) return alert("username must be at least 3 letter");
-    rChat.connect(identity, function(status) {
-        if (!status) {
-            console.err("Couldn't connect");
-            return;
-        }
-        else {
-            console.log("Connection successfull!");
-            initializeGroupChat();
-            rChat.onMessage(loadGroupMessageHistory, loadPrivateMessageHistory);
-            loadAvailableLanguages();
-            $(".intro-modal").modal('hide');
-            $("#username").html(identity);
-        }
-    });
-    return false;
-}
 
-function loadAvailableLanguages() {
-    translator.getLanguageList(function(languages){
-        var languageOption = "";
-        $.each(languages, function(key, value) {
-            languageOption += '<option value="' + key + '">' + value + '</option>';
-        });
-
-        var language = $("#language");
-
-        language.append(languageOption);
-        language.change(function(){
-            console.log("Changed language to"+language.val());
-            rChat.setLanguage(language.val());
-        });
-        language.val("en").trigger('change');
-    });
-}
-
-//********Group Chat Functions*************
-
-function initializeGroupChat() {
-    $(".container").on('click','.groupSendBtn',function(){
-        sendGroupMessage();
-        return false;
-    });
-	
-	$("#textToSend").keypress(function (e) {
-		if (e.which == 13) {
-			sendGroupMessage();
-			return false;
-		}
-	});
-	
-    rChat.joinGroup("UpStageCoder",loadGroupMembers, groupMemberLeaved, function(status){
-        rChat.getGroupMembers(loadGroupMembers);
-    });
-}
-
-function loadGroupMembers(members) {
-    var usersList = $(".onlineUserList");
-    /*$.each(members, function(index){
-        var endPoint = members[index].getEndpoint();
-        var userElement = usersList.find(".row:first").clone();
-        if(endPoint.id != rChat.userId && !$('.onlineUser').hasClass("user-"+endPoint.id)) {
-			userElement.addClass("user-"+endPoint.id);
-            userElement.find(".identityName").html(endPoint.id);
-            usersList.append(userElement);
-            userElement.click(function(){
-                enterPrivateChat(endPoint.id);
-            });
-        }
-    });*/
-}
-
-function groupMemberLeaved(member) {
-    var ep		= member.getEndpoint();
-	var user	= $(".user-"+ ep.id);
-    user.remove();
-    rChat.leavePrivateChat(ep.id, leftPrivateChat);
-}
-
-function sendGroupMessage() {
-    var messageObj  = {};
-    var messageText = $("#textToSend").val();
-    var language    = $("#language").val();
-    if (messageText.trim().length === 0) return;
-
-    messageObj["message"] = messageText;
-    messageObj["lang"]    = language;
-    messageObj["type"]    = 'text';
-    messageObj["genre"]   = 'group';
-    messageObj["timestamp"]   = Date.now();
-    //console.log(messageObj);
-    rChat.sendGroupMessage(messageObj, loadGroupMessageHistory);
-
-    //reset textbox
-    $("#textToSend").val("");
-}
-
-function loadGroupMessageHistory(sender, messageObj) {
-    var timestamp = new Date(messageObj.timestamp);
-    var newDate = new Date();
-    newDate.setTime(timestamp);
-    dateString = newDate.toLocaleTimeString();
-
-	//timestamp     = timestamp.getHours()+":"+timestamp.getMinutes()+":"+timestamp.getSeconds();
-	var messageType = "";
-	if(messageObj.type=="voice")
-		messageType = "(Voice) ";
-    var msgRows = $('.groupMsg');
-    var newRow = msgRows.last().clone();
-    newRow.find(".sender").text(sender);
-    newRow.find(".content").text(messageType+messageObj.message);
-	newRow.find(".msgInfo").attr("title","Original Language: "+messageObj.lang);
-	newRow.find(".timestamp").text(dateString);
-    msgRows.last().after(newRow);
-	$(".msgInfo").tooltip({container: 'body'});
-}
-
-//************* End Group Chat Functions ***************
+app.factory("getIdentity", function(){
+  return {dataIdentity: null }
+});
 
 
 
-//************** private chat functions ***************
+app.controller("UserListController", UserListController);
+app.controller("GroupController", GroupController);
+app.controller("LoginAttemptController", LoginAttemptController);
+app.controller("LoginBoxController", LoginBoxController);
+app.controller("LanguageController", LanguageController);
+app.controller("GroupMessageController", GroupMessageController);
+app.controller("PrivateMessageController", PrivateMessageController);
+app.controller("PrivateChatBoxesController", PrivateChatBoxesController);
 
-function enterPrivateChat(userId) {
-    rChat.joinPrivateChat(userId, function(){
-        console.log("Entered in a private chat successfully");
-        var privateChatBox = $('.privateChatBox');
-        if(!privateChatBox.hasClass("private-chat-"+userId)){
-            createPrivateChateBox(userId);
-        }
-        initAudioListener(userId);
-    });
-}
+app.directive("header", function(){
+	return {
+		templateUrl: "header.php"
+	};
+});
 
-function loadPrivateMessageHistory(sender, receiver, messageObj) {
-	
-    if(messageObj.type == "voice" && sender != "Me") {
-        robotSpeaker.speak(messageObj.lang, messageObj.message);
-    }
-	var privateChatBox  = $('.privateChatBox');
-	var privateChatname = 'private-chat-'+sender;
-	if(sender=="Me")
-		privateChatname = 'private-chat-'+receiver;
-	
-	if(!privateChatBox.hasClass(privateChatname)){
-        enterPrivateChat(sender);
-	}
-	var msgRows = $('.'+privateChatname +' .privateMsg');
-	var timestamp = new Date(messageObj.timestamp);
-    var newDate = new Date();
-    newDate.setTime(timestamp);
-    timeString = newDate.toLocaleTimeString();
-	//timestamp     = timestamp.getHours()+":"+timestamp.getMinutes()+":"+timestamp.getSeconds();
-	var messageType = "";
-	if(messageObj.type=="voice")
-		messageType = "(Voice) ";
-    var newRow = msgRows.first().clone();
-    newRow.find(".sender").text(sender);
-    newRow.find(".content").text(messageType+messageObj.message);
-	newRow.find(".timestamp").text(timeString);
-	//newRow.find(".msgInfo").attr("title","Original Language: "+messageObj.lang);
-	newRow.show();
-    msgRows.last().after(newRow);
-	$(".msgInfo").tooltip({container: 'body'});
-    
-}
-
-function createPrivateChateBox(userId){
-	var privateChatBox = $('.privateChatBox');
-    var newBox = privateChatBox.first().clone();
-	newBox.addClass("private-chat-"+userId);
-	newBox.find(".chatWith").text(userId);
-    privateChatBox.last().after(newBox);
-    //newBox.css('position', 'absolute').css('bottom','0px');
-	
-    newBox.find(".privateSendBtn").click(function(){
-		sendPrivateMessage(userId); 
-    });
-	
-	newBox.find(".privateTextToSend").keypress(function (e) {
-		if (e.which == 13) {
-			sendPrivateMessage(userId);
-		}
-	});
-	newBox.find(".closeBox").click(function(){
-		rChat.leavePrivateChat(userId, leftPrivateChat);
-    });
-
-}
-
-function leftPrivateChat(userId){
-    if(inAudioChat) {
-        stopListening($(".private-chat-" + userId + " .audio.stop"), userId)
-    }
-    $(".private-chat-"+userId).remove();
-}
-
-function sendPrivateMessage(userId) {
-    var messageObj  = {};
-	var privateTextArea = $(".private-chat-"+userId+" .privateTextToSend");
-    var messageText = privateTextArea.val();
-    var language    = $("#language").val();
-    if (messageText.trim().length === 0) return;
-
-    messageObj["message"] = messageText;
-    messageObj["lang"]    = language;
-    messageObj["type"]    = 'text';
-    messageObj["genre"]   = 'private';
-    messageObj["timestamp"]   = Date.now();
-    //console.log(messageObj);
-    rChat.sendPrivateMessage(messageObj, userId, loadPrivateMessageHistory);
-
-    //reset textbox
-    privateTextArea.val("");
-}
-
-//************** End private chat functions ***************
-
-
-//*************** Audio Chat *********************
-var inAudioChat = false;
-var listener;
-
-function initAudioListener(userId) {
-    var btn = $(".private-chat-" + userId + " .audio.start");
-    console.log("private audio call btn: " + btn.html());
-    btn.unbind('click');
-    btn.click(function(){
-        startListening(btn, userId);
-    });
-}
-
-function startListening(btn, userId) {
-    btn.removeClass("start").addClass("stop");
-    console.log("starting to listen audio input");
-    if (inAudioChat) {
-        alert("You are in a audio chat already. stop that first");
-        return;
-    }
-    listener = new AudioListener($("#language").val(), function(text){
-        sendRecognizedVoiceMessage(text, userId);
-    });
-    listener.listen();
-    btn.unbind('click');
-    inAudioChat = true;
-    btn.click(function () {
-        stopListening(btn, userId);
-    });
-}
-
-function sendRecognizedVoiceMessage(text, userId) {
-    console.log("audio: " + text+" sending to:"+userId);
-    rChat.sendPrivateMessage({
-        message: text, lang: $("#language").val(),
-        "type": "voice", "genre": "private", "timestamp": Date.now()
-    }, userId, loadPrivateMessageHistory);
-    if(!listener.isContinuous()){
-        stopListening($(".private-chat-" + userId + " .audio.stop"), userId);
-    }
-}
-
-function stopListening(btn, userId) {
-    listener.stop();
-    btn.removeClass("stop").addClass("start");
-    inAudioChat = false;
-    btn.click(function(){
-        startListening(btn, userId);
-    });
-}
-
-//*************** End Audio Chat *****************
+$(document).ready(function(){
+	//$('.carousel').carousel();
+});
